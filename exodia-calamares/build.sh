7192e29e87ebd05e60c864c51211593c92aca373
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-07-08 23:03:25 +0200

    updated PKGBUILDs

---
+++ exodia-calamares/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#
+# Copyright (C) 2022 Mahmoud Mohamed (Ozil)  <https://github.com/mmsaeed509>
+# LICENSE © GNU-GPL3
+#
+
+echo ""
+echo "############################"
+echo "# Making the package...... #"
+echo "############################"
+echo ""
+
+makepkg -sc --noconfirm
+
+echo ""
+echo "####################"
+echo "#     D O N E!     #"
+echo "####################"
+echo ""
